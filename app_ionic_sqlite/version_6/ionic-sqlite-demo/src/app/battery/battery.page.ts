7d0f61b3955b3e972bbc8952602ae8c6587e018c
LuEnCam <67584779+LuEnCam@users.noreply.github.com>
2023-01-30 16:55:18 +0100

    new deploy on android

--- app_ionic_sqlite/version_6/ionic-sqlite-demo/src/app/battery/battery.page.ts
+++ app_ionic_sqlite/version_6/ionic-sqlite-demo/src/app/battery/battery.page.ts
@@ -2,6 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { DatabaseService } from '../database.service';
 import { Platform } from '@ionic/angular';
 
+import {
+  ActionPerformed,
+  PushNotificationSchema,
+  PushNotifications,
+  Token,
+} from '@capacitor/push-notifications';
+
 @Component({
   selector: 'app-battery',
   templateUrl: './battery.page.html',
@@ -41,7 +48,42 @@ export class BatteryPage implements OnInit {
     });
    }
 
-  ngOnInit() {
+   ngOnInit() {
+/*     console.log('Initializing HomePage');
+
+    // Request permission to use push notifications
+    // iOS will prompt user and return if they granted permission or not
+    // Android will just grant without prompting
+    PushNotifications.requestPermissions().then(result => {
+      if (result.receive === 'granted') {
+        // Register with Apple / Google to receive push via APNS/FCM
+        PushNotifications.register();
+      } else {
+        // Show some error
+      }
+    });
+
+    PushNotifications.addListener('registration', (token: Token) => {
+      alert('Push registration success, token: ' + token.value);
+    });
+
+    PushNotifications.addListener('registrationError', (error: any) => {
+      alert('Error on registration: ' + JSON.stringify(error));
+    });
+
+    PushNotifications.addListener(
+      'pushNotificationReceived',
+      (notification: PushNotificationSchema) => {
+        alert('Push received: ' + JSON.stringify(notification));
+      },
+    );
+
+    PushNotifications.addListener(
+      'pushNotificationActionPerformed',
+      (notification: ActionPerformed) => {
+        alert('Push action performed: ' + JSON.stringify(notification));
+      },
+    ); */
   }
 
   getAllDevices() {
@@ -144,6 +186,7 @@ export class BatteryPage implements OnInit {
       this.batteryInput.number_of_cycles = _battery.number_of_cycles;
       this.batteryInput.last_cycle_date = _battery.last_cycle_date;
       this.addOrEdit = "Edit";
+      this.inputMode = true;
     }
 
     async deleteBattery(_battery : any) {
